eb46fe171b3de81aae302318415da23e3d8ec10c
“Arpita <arpitakumari10653@gmail.com>
2025-05-29 18:10:39 +0530

    there is no change if language will be same

---
+++ 8_problem.py
@@ -0,0 +1,19 @@
+d = {}
+
+name = input("Enter friend name: ")
+language = input("Enter language: ")
+d.update({name:language})
+
+name = input("Enter friend name: ")
+language = input("Enter language: ")
+d.update({name:language})
+
+name = input("Enter friend name: ")
+language = input("Enter language: ")
+d.update({name:language})
+
+name = input("Enter friend name: ")
+language = input("Enter language: ")
+d.update({name:language})
+
+print(d)
